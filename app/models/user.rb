30ae29b126c19daec03c0511aba2b03b6c2c769e
luis frade <luis.carvalho.frade@gmail.com>
2019-11-25 19:55:50 -0300

    Models done

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :owned_pets, foreign_key: :owner_id, class_name: "Pet"
 end
