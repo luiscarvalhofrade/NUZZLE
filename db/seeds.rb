aaa13eda643652e34525775737b6e26b8acf33b1
Krista Calleja <kristacm@gmail.com>
2019-11-26 17:51:37 -0300

    Added seed animals with Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+puts 'Creating owner'
+owner = User.create!(
+  email: Faker::Internet.email,
+  password: '123456'
+  )
+
+puts 'Inserting 5 fictitious animals...'
+5.times do |pet|
+  pet = Pet.create!(
+    name: Faker::Creature::Animal.name,
+    description: Faker::Lorem.sentence,
+    price: Faker::Number.decimal(l_digits: 2),
+    owner: owner
+    )
+end
+
+puts 'Ended seeding!'
